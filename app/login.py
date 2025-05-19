c6d3497ef94dd5754386719010c6074fe8abb364
Muzdalifah20 <muzdalifahsuliman@gmail.com>
2025-05-19 17:59:58 +0300

    Update login.py

--- app/login.py
+++ app/login.py
@@ -7,6 +7,15 @@ def hashing(password):
     for char in password:
         hashed += ord(char)
     return str(hashed)
+
+@app.after_request
+def add_security_headers(response):
+        response.headers["Cache-Control"] = (
+            "no-store, no-cache, must-revalidate, max-age=0"
+        )
+        response.headers["Pragma"] = "no-cache"
+        response.headers["Expires"] = "0"
+    return response
 # # do the login
 def do_login():
     email = request.form.get("email", "").strip().lower()
